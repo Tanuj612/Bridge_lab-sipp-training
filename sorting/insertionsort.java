f3b08e7c72775e0a91fe7aecfc35105a70a636e7
Tanuj612 <tanujmittal2018@gmail.com>
2025-07-10 09:23:01 +0530

    sorting

---
+++ sorting/insertionsort.java
@@ -0,0 +1,18 @@
+public class insertionsort {
+    public static void main(String[] args) {
+
+            int[] data = {5, 2, 9, 1, 5, 6};
+            public static void insertionSort(int[] arr) {
+                for (int i = 1; i < arr.length; i++) {
+                    int key = arr[i];
+                    int j = i - 1;
+                    while (j >= 0 && arr[j] > key) {
+                        arr[j + 1] = arr[j];
+                        j--;
+                    }
+                    arr[j + 1] = key;
+                }
+            }
+        }
+    }
+
