8cd388db0bcca8ba7b8d51e41f5bd388d448aa39
Tanuj612 <tanujmittal2018@gmail.com>
2025-07-22 01:47:26 +0530

    change

--- Level_1/q5.java
+++
@@ -1,9 +0,0 @@
-package Level_1;
-import java.util.*;
-public class q5 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt() % sc.nextInt());
-        sc.close();
-    }
-}
--- array/BMI.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class BMI {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        
-    }
-}
